2a8f744de1d0759ea6d22bd589990acc2e2485f3
miguelx46 <139667111+miguelx46@users.noreply.github.com>
2024-10-03 16:11:58 -0500

    Test de ollama

---
+++ Main.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //variables para el modelo
+        String nombremodelo = "gemma2:3b";
+        String promptText = "What is milk?";
+        //url
+        URL url = new URL("http:localhost:11434/api/generate");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json; utf-8");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setDoOutput(true);
+
+        String jsonInputString = String.format(
+                "{\"model\": \"%s\", \"prompt\": \"%s\", \"stream\": false}", nombremodelo, promptText
+        );
+
+        try (OutputStream os = conn.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+            os.write(input, 0, input.length);
+        }
+        int code = conn.getResponseCode();
+        System.out.println("Codigo de respuesta: " + code);
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+        StringBuilder response = new StringBuilder();
+        String line;
+        while ((line = in.readLine()) != null) {
+            response.append(line);
+        }
+        in.close();
+
+        //imprimir la respuesta
+        System.out.println("Response body "+ response.toString());
+        JSONObject jsonResponse = new JSONObject(response.toString());
+        String responseText = jsonResponse.getString("response");
+        System.out.println("Response "+ responseText);
+
+        //cerrar la conexión
+        conn.disconnect();
+    }
+}
