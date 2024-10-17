e7738085aabad7ce97cdbdf626e59803f72303d3
miguelx46 <139667111+miguelx46@users.noreply.github.com>
2024-10-17 18:56:01 -0500

    Add files via upload

---
+++ Ejercicio_GUI_JOSECURY/src/ejercicio_gui_josecury/Ejercicio_GUI_JOSECURY.java
@@ -0,0 +1,14 @@
+
+package ejercicio_gui_josecury;
+
+
+public class Ejercicio_GUI_JOSECURY {
+
+    
+    
+    public static void main(String[] args) {   
+        JTABLEGUI frame = new JTABLEGUI();
+        frame.setVisible(true);
+    }
+    
+}
